b09a7575f4f054f22e83db2244ee2b86e4fc68f7
Dana Kulp <dkulp23@gmail.com>
2017-03-30 21:36:28 -0700

    Updated gallery service to include update and delete methods.

--- app/service/gallery-service.js
+++ app/service/gallery-service.js
@@ -88,5 +88,68 @@ function galleryService($q, $log, $http, authService) {
     });
   };
 
+  service.updateGalleries = function(galleryID, galleryData) {
+    $log.debug('galleryService.updateGalleries');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`; //eslint-disable-line
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      };
+
+      return $http.put(url, galleryData, config);
+    })
+    .then( res => {
+      for (let i = 0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if (current._id === galleryID) {
+          service.gallery[i] = res.data;
+          break;
+        }
+      }
+
+      return res.data;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.deleteGallery = function(galleryID) {
+    $log.debug('galleryService.deleteGallery');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`; //eslint-disable-line
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      $log.log(res);
+      for (let i=0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if (current._id === galleryID) {
+          service.galleries.splice(i, 1);
+          break;
+        }
+      }
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
   return service;
 }
